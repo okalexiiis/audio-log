37f13b03d03050e2378f9b85dd6d7e683f0c729a
Alexiiis <okalexiiis@gmail.com>
2025-11-09 21:04:09 -0700

    Yeah Logger middleware

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
+import "dotenv/config";
 import { Hono } from "hono";
 import { UserDrizzleRepository } from "./lib/users/services/repositories/user.repository";
+import { Logger, TLOG_LEVEL } from "./core/utils/logger";
+import { requestLogger } from "./core/middlewares";
 
 const app = new Hono();
+const log_level = process.env.LOG_LEVEL! as TLOG_LEVEL;
+const logger = new Logger(log_level);
+
+// Pre-Request Middlewares
+app.use("*", requestLogger(logger));
 
 app.get("/", (c) => {
   return c.json({ message: "Hello World" });
