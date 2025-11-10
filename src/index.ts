a4e552eef4d3377d1cf06e99b0bf7d68cdd77a9d
Alexiiis <okalexiiis@gmail.com>
2025-11-10 12:04:55 -0700

    Implementation of those routes

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import { Hono } from "hono";
 import { UserDrizzleRepository } from "./lib/users/services/repositories/user.repository";
 import { Logger, TLOG_LEVEL } from "./core/utils/logger";
 import { requestLogger } from "./core/middlewares";
+import UserRouter from "./lib/users/routes";
+import { DomainError } from "./core/types/domain-errors";
 
 const app = new Hono();
 const log_level = process.env.LOG_LEVEL! as TLOG_LEVEL;
@@ -10,6 +12,8 @@ const logger = new Logger(log_level);
 
 // Pre-Request Middlewares
 app.use("*", requestLogger(logger));
+
+app.route("/", UserRouter);
 
 app.get("/", (c) => {
   return c.json({ message: "Hello World" });
@@ -23,8 +27,14 @@ app.get("/test", async (c) => {
   return c.json({ data: users });
 });
 
-app.onError((err, c) => {
-  console.log(err);
+app.onError((err: any, c) => {
+  logger.danger("CATCH ERROR", err.message, err);
+  if (err instanceof DomainError) {
+    return c.json(
+      { message: err.message, errors: err.validationErrors },
+      err.status,
+    );
+  }
   return c.json({ cause: err.cause, message: err.message, name: err.name });
 });
 
