7ed0ccfb0da5e8b2228fe5b44e31cb07a936d374
Alexiiis <okalexiiis@gmail.com>
2025-11-09 18:57:37 -0700

    just a route for easy testing

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,23 @@
 import { Hono } from "hono";
+import { UserDrizzleRepository } from "./lib/users/services/repositories/user.repository";
 
 const app = new Hono();
 
 app.get("/", (c) => {
   return c.json({ message: "Hello World" });
+});
+
+app.get("/test", async (c) => {
+  const repo = new UserDrizzleRepository();
+
+  const users = await repo.findAll({}, { username: "ito" });
+
+  return c.json({ data: users });
+});
+
+app.onError((err, c) => {
+  console.log(err);
+  return c.json({ cause: err.cause, message: err.message, name: err.name });
 });
 
 export default app;
