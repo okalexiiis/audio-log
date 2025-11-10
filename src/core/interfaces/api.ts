531c2108ec317e0a9249e1e097fc57ca602b7857
Alexiiis <okalexiiis@gmail.com>
2025-11-09 18:51:10 -0700

    added new fields for  pagination params
    added a generic, added a orderBy field, added a sort field, made all the
    fields optional

--- src/core/interfaces/api.ts
+++ src/core/interfaces/api.ts
@@ -1,4 +1,6 @@
-export interface PaginationParams {
-  page: number;
-  limit: number;
-}
+export type PaginationParams<T> = {
+  page?: number | 1;
+  limit?: number | 10;
+  orderBy?: keyof T;
+  sort?: "asc" | "desc";
+};
