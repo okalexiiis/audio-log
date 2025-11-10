1c42e41047d2cc0b9a145782ca22947a7e72f3ea
Alexiiis <okalexiiis@gmail.com>
2025-11-09 18:48:14 -0700

    Rename de File to User

--- src/lib/users/models/user.ts
+++
@@ -1,9 +0,0 @@
-import { index, integer, pgTable, varchar } from "drizzle-orm/pg-core";
-
-export const User = pgTable("users", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  username: varchar({ length: 25 }).notNull().unique(),
-  email: varchar({ length: 255 }).notNull().unique(),
-  password_hash: varchar({ length: 255 }).notNull(),
-  avatar_url: varchar({ length: 255 }),
-});
