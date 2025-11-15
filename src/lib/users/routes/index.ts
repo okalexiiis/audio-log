1ea118364f57a1f869c3559f86bd77c8d1320c47
Alexiiis <okalexiiis@gmail.com>
2025-11-15 16:58:30 -0700

    routes for the user by username use case

--- src/lib/users/routes/index.ts
+++ src/lib/users/routes/index.ts
@@ -1,5 +1,6 @@
 import { Context, Hono } from "hono";
 import { RegisterUser } from "../services/use-cases/register";
+import { SearchUserByUsername } from "../services/use-cases/search-user-by-username";
 
 const app = new Hono();
 
@@ -7,7 +8,14 @@ app.post("/user/register", async (c: Context) => {
   const uc = new RegisterUser();
   const raw = await c.req.json();
   const res = await uc.execute(raw);
-  return c.json(res);
+  return c.json(res, 201);
+});
+
+app.get("/user/:username", async (c: Context) => {
+  const uc = new SearchUserByUsername();
+  const raw = c.req.param("username");
+  const res = await uc.execute(raw);
+  return c.json(res, 200);
 });
 
 export default app;
